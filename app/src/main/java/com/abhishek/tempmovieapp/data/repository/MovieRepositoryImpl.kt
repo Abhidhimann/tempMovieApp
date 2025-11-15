41cc8755c2acf48a2c931fc4cd28d88cadc0ea07
Abhishek <abhishekdhimaniitg@gmail.com>
2025-11-15 17:16:17 +0900

    saving movies in db

--- app/src/main/java/com/abhishek/tempmovieapp/data/repository/MovieRepositoryImpl.kt
+++ app/src/main/java/com/abhishek/tempmovieapp/data/repository/MovieRepositoryImpl.kt
@@ -2,7 +2,8 @@ package com.abhishek.tempmovieapp.data.repository
 
 import android.util.Log
 import com.abhishek.tempmovieapp.core.constants.classTag
-import com.abhishek.tempmovieapp.data.mapper.toDomain
+import com.abhishek.tempmovieapp.data.local.dao.MovieDao
+import com.abhishek.tempmovieapp.data.mapper.toEntity
 import com.abhishek.tempmovieapp.data.remote.api.MovieApi
 import com.abhishek.tempmovieapp.domain.model.Movie
 import com.abhishek.tempmovieapp.domain.repository.MovieRepository
@@ -15,6 +16,7 @@ import javax.inject.Inject
 
 class MovieRepositoryImpl @Inject constructor(
     private val movieApi: MovieApi,
+    private val movieDao: MovieDao,
     private val dispatcher: CoroutineDispatcher = Dispatchers.IO
 ) : MovieRepository {
     override suspend fun refreshTrendingMovies(): Result<Unit> {
@@ -23,10 +25,8 @@ class MovieRepositoryImpl @Inject constructor(
                 val response = movieApi.getTrendingMovies()
                 if (response.isSuccessful) {
                     val moviesDto = response.body()?.results ?: emptyList()
-                    val movies = moviesDto.map { it.toDomain() }
-
-                    // todo save it local
-                    Log.i(classTag(), "Fetched movies from network $movies")
+                    movieDao.insertMovies(moviesDto.map { it.toEntity() })
+                    Log.i(classTag(), "Fetched movies from network $moviesDto")
                     Result.success(Unit)
                 } else {
                     // can map different code to different error enums then later handle they in ui
